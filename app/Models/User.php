3eb0606f8357e38a607f9949eb27b84e0baec7bc
Abdul Rachmat <idsumoblog@gmail.com>
2025-08-21 12:01:12 +0700

    user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,6 +8,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
@@ -142,6 +143,12 @@ class User extends Authenticatable
         return $this->position?->department_id;
     }
 
+    protected function name(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => ucwords(strtolower($value))
+        );
+    }
     public function fcmTokens()
     {
         return $this->hasMany(UserFcmToken::class);
